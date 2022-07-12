37c92da469ce8f220a51ee50b12fa63d076a2ccf
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-07-12 16:09:00 -0300

    Update JavaScript.js

--- Operações/Linguagens/JavaScript.js
+++ Operações/Linguagens/JavaScript.js
@@ -1,1 +1,41 @@
+//Criação de Variáveis
+a = 3
+b = 2 
 
+
+/*
+- Para acessar os valores do console.log é só acessar a saída/terminal no compilador
+- Se você acessar pelo navegador com o html, faça os passos que estarão no parenteses (Botão direito na tela do navegador > inspecionar > console)
+*/
+
+
+//Adição
+console.log(a + b)
+
+//Subtração
+console.log(a - b)
+
+//Multiplicação
+console.log(a * b)
+
+//Divisão
+console.log(a / b)
+
+//Se você deseja acessar o resto da divisão
+console.log(a % b)
+
+//Exponenciação | O segundo número será o exponencial
+console.log(a ** b)
+
+
+//Igual que
+console.log(a == b)
+
+//Diferente que
+console.log(a != b)
+
+//Maior que
+console.log(a >= b)
+
+//Menor que
+console.log(a <= b)
