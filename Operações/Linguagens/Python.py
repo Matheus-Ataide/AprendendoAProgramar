04d84f41f16ad8ee94aeef29de07fa7bd2b2a4e3
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-07-13 09:00:20 -0300

    Update Python.py

--- Operações/Linguagens/Python.py
+++ Operações/Linguagens/Python.py
@@ -1,1 +1,48 @@
+#Criação de Variáveis
+a = 3
+b = 2 
 
+
+# - Para acessar os valores do print é só acessar a saída/terminal no compilador
+# - Se você acessar pelo navegador com o html, faça os passos que estarão no parenteses (Botão direito na tela do navegador > inspecionar > console)
+
+
+#Adição
+print(a + b)
+
+#Subtração
+print(a - b)
+
+#Multiplicação
+print(a * b)
+
+#Divisão
+print(a / b)
+
+#Se você deseja acessar o resto da divisão
+print(a % b)
+
+#Exponenciação | O segundo número será o exponencial
+print(a ** b)
+
+
+#Você irá perceber que ao compilar, estará false/true, o programa está informando o resultado dessas análises que estão sendo pedidas
+
+#Igual que
+print(a == b)
+
+#Diferente que
+print(a != b)
+
+#Maior que
+print(a >= b)
+
+#Menor que
+print(a <= b)
+
+
+#Imprimindo os valores lá no HMTL
+somac = document.getElementById("soma")
+#document.getElementById("sub").textContent = document.getElementById("txt").innerHTML
+#document.getElementById("mult").textContent = document.getElementById("txt").innerHTML
+#document.getElementById("divi").textContent = document.getElementById("txt").innerHTML
