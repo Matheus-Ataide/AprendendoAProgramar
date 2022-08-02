69c58607bccbf157e1d8512d130e9472639b9e73
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-08-02 14:03:15 -0300

    Create Go.go

---
+++ Operações/Go.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+
+func main() {
+
+  //Criação de Variáveis
+  var a = 3
+  var b = 2 
+  
+  // Para acessar os valores do print é só acessar a saída/terminal no compilador
+  // Se você acessar pelo navegador com o html, faça os passos que estarão no parenteses (Botão direito na tela do navegador > inspecionar > console)
+  
+  
+  //Adição
+  fmt.Println(a + b)
+  
+  //Subtração
+  fmt.Println(a - b)
+  
+  //Multiplicação
+  fmt.Println(a * b)
+  
+  //Divisão
+  fmt.Println(a / b)
+  
+  //Se você deseja acessar o resto da divisão
+  fmt.Println(a % b)
+}
